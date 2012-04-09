de1efab25df667f594a9716e7a72c6a387e7a6d9
Coders Manic <manicstreetcoders@gmail.com>
2012-04-09 13:58:49 +0900

    added :dependent => :destroy to Post's :comments

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ActiveRecord::Base
 	validates :name, :presence => true
 	validates :title, :presence => true, :length => { :minimum => 2 }
 	
-	has_many :comments
+	has_many :comments, :dependent => :destroy
 end
