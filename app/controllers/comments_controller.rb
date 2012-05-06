6e699ff54ee1e9febb3a92a7506a215a8e762a67
Coders Manic <manicstreetcoders@gmail.com>
2012-05-06 17:40:19 +0900

    added debug(params)

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,6 +1,5 @@
 class CommentsController < ApplicationController
   def create
-    puts params
     @post = Post.find(params[:post_id])
     @comment = @post.comments.create(params[:comment])
     redirect_to post_path(@post)
